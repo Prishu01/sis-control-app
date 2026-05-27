2c0cb3ea98121d3612c980305becb699f6cc4ee7
Rishu Pandey <167758931+Prishu01@users.noreply.github.com>
2026-05-27 19:04:07 +0530

    Initial commit

--- App.tsx
+++ App.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
 import { BluetoothProvider } from './context/BluetoothContext';
+import BootSplash from 'react-native-bootsplash';
 
 export default function App() {
   return (
     <BluetoothProvider>
-      <NavigationContainer>
+      <NavigationContainer onReady={() => setTimeout(() => BootSplash.hide({ fade: true }), 1000)}>
         <AppNavigator />
       </NavigationContainer>
     </BluetoothProvider>
